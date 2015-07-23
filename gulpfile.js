1321872bc1abba77b5709a6c4f30aeb31e5df3af
Matt Venables <mattvenables@gmail.com>
2015-07-22 22:37:33 -0400

    Add jscs checking

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 'use strict';
 
+var jscs = require('gulp-jscs');
 var jshint = require('gulp-jshint');
 var gulp = require('gulp');
+var src = ['./*.js', './src/**/*.js', './test/**/*.js'];
 
-gulp.task('default', ['jshint']);
+gulp.task('default', ['jshint', 'jscs']);
 
 gulp.task('jshint', function() {
-  gulp.src(['./src/**/*.js', './test/**/*.js'])
+  gulp.src(src)
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('jscs', function() {
+  gulp.src(src)
+    .pipe(jscs());
 });
