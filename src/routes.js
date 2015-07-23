25585fb8335a2067537a78b2b7fdb8fe20e85668
Matt Venables <mattvenables@gmail.com>
2015-07-22 23:15:11 -0400

    Add users controller and routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var controllers = require('./controllers');
 var router = require('koa-router')();
+
+router.get('/users', controllers.users.index);
+router.get('/users/:id', controllers.users.show);
 
 module.exports = router;
