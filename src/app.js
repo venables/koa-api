6558ae02103d801bd5d584309918e10adc59f304
Matt Venables <mattvenables@gmail.com>
2015-07-22 21:50:01 -0400

    Add koa-helmet for secure headers

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@
 
 var app = require('koa')();
 var config = require('./config');
+var helmet = require('koa-helmet');
 var logger = require('koa-morgan').middleware;
 var middleware = require('./middleware');
 
 app.use(logger(config.logging.format));
+app.use(helmet());
 app.use(middleware.config);
 
 module.exports = app;
