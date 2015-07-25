299e0fc36563c9f85d3f0a38f779b42f9fbb8301
Matt Venables <mattvenables@gmail.com>
2015-07-25 17:56:43 -0400

    Use koa-json-body for body parsing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var app = require('koa')();
-var body = require('koa-bodyparser');
+var body = require('koa-json-body');
 var config = require('./config');
 var helmet = require('koa-helmet');
 var json = require('koa-json');
