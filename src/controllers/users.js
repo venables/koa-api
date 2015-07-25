9363155eae382d340723b49c9ef06e8bed1fb229
Matt Venables <mattvenables@gmail.com>
2015-07-25 16:28:40 -0400

    Add PUT, DELETE methods for users controller

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -3,44 +3,72 @@
 var _ = require('lodash');
 
 var index = function *() {
-  var records;
   var query = this.models.User.collection().query(function(knex) {
     knex.limit(50).offset(0).orderBy('created_at', 'asc');
   });
 
-  records = yield query.fetch();
+  var users = yield query.fetch();
 
   this.body = {
-    users: records.toJSON()
+    users: users.toJSON()
   };
 };
 
 var show = function *() {
-  var model = new this.models.User({ id: this.params.id });
-  var record = yield model.fetch();
+  var user = yield _fetchUser(this);
 
   this.body = {
-    user: record.toJSON()
+    user: user.toJSON()
   };
 };
 
 var create = function *() {
-  var model = new this.models.User(_userParams(this.request.body));
+  var user = new this.models.User(_userCreateParams(this.request.body));
 
-  yield model.save();
+  yield user.save();
 
   this.status = 201;
   this.body = {
-    user: model.toJSON()
+    user: user.toJSON()
   };
 };
 
-function _userParams(body) {
+var update = function *() {
+  var user = yield _fetchUser(this);
+
+  yield user.save(_userUpdateParams(this.request.body));
+
+  this.status = 200;
+  this.body = {
+    user: user.toJSON()
+  };
+};
+
+var destroy = function *() {
+  var user = yield _fetchUser(this);
+
+  yield user.destroy();
+
+  this.status = 204;
+};
+
+function *_fetchUser(ctx) {
+  var model = new ctx.models.User({ id: ctx.params.id });
+  return yield model.fetch({ require: true });
+}
+
+function _userCreateParams(body) {
+  return _.pick(body, 'email', 'password');
+}
+
+function _userUpdateParams(body) {
   return _.pick(body, 'email', 'password');
 }
 
 module.exports = {
   index: index,
   show: show,
-  create: create
+  create: create,
+  update: update,
+  destroy: destroy
 };
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ var router = require('koa-router')();
 router.get('/users', controllers.users.index);
 router.get('/users/:id', controllers.users.show);
 router.post('/users', controllers.users.create);
+router.put('/users/:id', controllers.users.update);
+router.del('/users/:id', controllers.users.destroy);
 
 router.post('/sessions', controllers.sessions.create);
 
