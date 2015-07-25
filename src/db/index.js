eaf1fc85d136262b22dd4c46f09e4c7ab77909e4
Matt Venables <mattvenables@gmail.com>
2015-07-24 23:53:47 -0400

    Cleaner password plugin integration

--- src/db/index.js
+++ src/db/index.js
@@ -4,12 +4,10 @@ var config = require('../config');
 var knexfile = require('../../knexfile')[config.env];
 var knex = require('knex')(knexfile);
 var bookshelf = require('bookshelf')(knex);
-var plugins = require('./plugins');
 
 bookshelf.plugin('registry');
 bookshelf.plugin('virtuals');
 bookshelf.plugin('visibility');
-
-plugins.hasPasswordDigest(bookshelf);
+bookshelf.plugin(require('./plugins/hasPasswordDigest'));
 
 module.exports = bookshelf;
